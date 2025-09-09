b7f59c7346279b7a83df004aece6d8f152bf26f3
Carlos Erick Mota Dos Santos <carloserico71@gmail.com>
2025-09-09 02:30:00 -0300

    feat: dtos for requests and responses

---
+++ src/application/StockManager.Application/Dtos/requests/ActionUserDto.cs
@@ -0,0 +1,7 @@
+namespace application.StockManager.Application.Dtos.resquests;
+
+public class ActionUserDto
+{
+    // public Guid userId { get; set; }
+    public Guid productId { get; set; }
+}
---
+++ src/application/StockManager.Application/Dtos/responses/OperationCompletedResponseDtp.cs
@@ -0,0 +1,10 @@
+using application.StockManager.Application.Dtos;
+
+namespace application.StockManager.Application.responses
+{
+    public class OperationCompletedResponseDto : ResultResponseBase
+    {
+        public override int statusCode { get; set; }
+        public override string message { get; set; }
+    }
+}
