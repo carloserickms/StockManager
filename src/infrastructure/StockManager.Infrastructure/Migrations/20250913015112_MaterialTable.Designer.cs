91e8f7034bce776125c2a07539c3f4558f7a1ae4
Carlos Erick Mota Dos Santos <carloserico71@gmail.com>
2025-09-13 00:42:53 -0300

    feat: new database migration, with relationships with colors and materials table

--- src/infrastructure/StockManager.Infrastructure/Migrations/20250913015112_MaterialTable.Designer.cs
+++ src/infrastructure/StockManager.Infrastructure/Migrations/20250913015112_MaterialTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using infrastructure.StockManager.Infrastructure.Persistence;
 
@@ -11,9 +12,11 @@ using infrastructure.StockManager.Infrastructure.Persistence;
 namespace StockManager.Infrastructure.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250913015112_MaterialTable")]
+    partial class MaterialTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
