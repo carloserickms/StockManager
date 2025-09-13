c69d56836bcb71c172f9c08ffbcb775014e48ab1
Carlos Erick Mota Dos Santos <carloserico71@gmail.com>
2025-09-13 00:43:48 -0300

    feat: creating a contract for communication between layers

---
+++ src/application/StockManager.Application/Interfaces/IMaterialSerivice.cs
@@ -0,0 +1,12 @@
+using application.StockManager.Application.Dtos;
+using application.StockManager.Application.Dtos.resquests;
+using domain.StockManager.Domain.Entities;
+
+namespace application.StockManager.Application.Interfaces
+{
+    public interface IMaterialSerivice
+    {
+        public Task<ResultResponseBase> CreateMaterial(Material product, List<Guid>? colorIds);
+        public Task<ResultResponseBase> DeleteMaterial(ActionUserDto actionUser);
+    }
+}
--- src/application/StockManager.Application/Interfaces/IProductService.cs
+++ src/application/StockManager.Application/Interfaces/IProductService.cs
@@ -6,7 +6,7 @@ namespace application.StockManager.Application.Interfaces
 {
     public interface IProductService
     {
-        public Task<ResultResponseBase> CreateProduct(Product product, List<Guid>? materialIds, List<Guid>? colorIds);
+        public Task<ResultResponseBase> CreateProduct(Product product, List<MaterialDto>? materialIds, List<Guid>? colorIds);
         public Task<ResultResponseBase> DeleteProduct(ActionUserDto actionUser);
     }
 }
