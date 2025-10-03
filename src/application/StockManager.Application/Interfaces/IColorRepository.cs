619830230d603840e8dff3b4505a302d46f672be
Carlos Erick Mota Dos Santos <carloserico71@gmail.com>
2025-10-03 01:49:09 -0300

    refactor: remove unused repository and service interfaces; clean up migration files

--- src/application/StockManager.Application/Interfaces/IColorRepository.cs
+++
@@ -1,9 +0,0 @@
-using domain.StockManager.Domain.Entities;
-
-namespace application.StockManager.Application.Interfaces
-{
-    public interface IColorRepository : IRepository<Color>
-    {
-        Task<IEnumerable<Color>> GetAllColorsOnTheList(List<int> colors);
-    }
-}
--- src/application/StockManager.Application/Interfaces/IProductRepository.cs
+++
@@ -1,9 +0,0 @@
-using domain.StockManager.Domain.Entities;
-
-namespace application.StockManager.Application.Interfaces
-{
-    public interface IProductRepository : IRepository<Product>
-    {
-
-    }
-}
